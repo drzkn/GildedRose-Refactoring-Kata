dbc98b74df02db92be0fbf0d8124f977f33137f2
drzkn <diegorazq@gmail.com>
2023-11-24 11:46:38 +0100

    test: traduce los requerimientos

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,48 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+  it(`cuando expira un producto, se degrada la calidad con el doble de velocidad`, () => {
+
+  })
+
+  it(`la calidad de un artículo nunca puede ser negativa`, () => {
+
+  })
+
+  it(`la calidad de un artículo nunca será mayor que 50`, () => {
+
+  })
+
+  it(`el artículo Sulfuras no modifica su fecha de expiracion ni su calidad`, () => {
+
+  })
+
+  describe('el queso brie', () => {
+    it(`aumenta su calidad en lugar de disminuir`, () => {
+
+    })
+
+    it(`aumenta su calidad el doble en lugar de disminuir, cuando expira`, () => {
+
+    })
+  })
+
+  describe('las entradas al backstage', () => {
+    it(`aumenta su calidad cuando se acerca la expiración`, () => {
+
+    })
+
+    it(`si la expiracion es menor que 10, aumenta su calidad en 2`, () => {
+
+    })
+
+    it(`si la expiración es menor que 5, aumenta su calidad en 3`, () => {
+
+    })
+
+    it(`si expira, la calidad cae a 0`, () => {
+
+    })
+  })
 });
+
